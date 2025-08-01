5faa7bc411cbaff9441fd907d5758b719d5acde2
NancyGupta04 <nancygupta884@gmail.com>
2025-08-01 20:56:58 +0530

    6th_commit

--- src/components/core/Dashboard/AddCourse/CourseBuilder/NestedView.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilder/NestedView.jsx
@@ -26,11 +26,14 @@ export default function NestedView({ handleChangeEditSectionName }) {
   const [confirmationModal, setConfirmationModal] = useState(null)
 
   const handleDeleleSection = async (sectionId) => {
-    const result = await deleteSection({
+    const result = await deleteSection(
+      {
       sectionId,
       courseId: course._id,
-      token,
-    })
+    },
+      token
+
+  )
     if (result) {
       dispatch(setCourse(result))
     }
@@ -38,7 +41,7 @@ export default function NestedView({ handleChangeEditSectionName }) {
   }
 
   const handleDeleteSubSection = async (subSectionId, sectionId) => {
-    const result = await deleteSubSection({ subSectionId, sectionId, token })
+    const result = await deleteSubSection({ subSectionId, sectionId }, token)
     if (result) {
       // update the structure of course
       const updatedCourseContent = course.courseContent.map((section) =>
